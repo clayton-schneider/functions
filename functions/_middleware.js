fb4ae918e27ffda22ae9ab7f65ee213735aa06b1
clayton-schneider <schneiderc2016@gmail.com>
2022-11-08 22:57:42 -0500

    added error handler

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,15 +1,24 @@
 import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest = mailchannelsPlugin({
-  personalizations: [
-    {
-      to: [{ name: "ACME Support", email: "hello@example.com" }],
-    },
-  ],
-  from: { name: "Enquiry", email: "no-reply@example.com" },
-  respondWith: () =>
-    new Response(null, {
-      status: 302,
-      headers: { Location: "/thank-you" },
-    }),
-});
+const errorHandler = async ({ next }) => {
+  try {
+    return await next();
+  } catch (err) {
+    return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+  }
+};
+
+const mailMiddleware = (context) =>
+  mailchannelsPlugin({
+    personalizations: [
+      {
+        to: [{ name: "ACME Support", email: "hello@example.com" }],
+      },
+    ],
+    from: { name: "Enquiry", email: "no-reply@example.com" },
+    respondWith: () =>
+      new Response(null, {
+        status: 302,
+        headers: { Location: "/thank-you" },
+      }),
+  });
